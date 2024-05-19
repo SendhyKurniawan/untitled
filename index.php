3e44ed4453527048538e2871af0902f64de61f09
Sendhy Kurniawan <53243422+SendhyKurniawan@users.noreply.github.com>
2024-05-20 00:22:14 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>PPKS</title>
+</head>
+
+<body>
+    <!-- Nav -->
+    <nav class="navbar" id="navbar">
+        <a href="#" class="navlogo">
+            <img src="logo.png" alt="Untitled">
+        </a>
+        <ul class="nav-menu">
+            <li><a href="#">Beranda</a></li>
+            <li><a href="#">Lapor Segera!</a></li>
+            <li class="dropdown">
+                <div class="dropbtn">
+                    <a href="#">Kekerasan Seksual</a>
+                    <div class="arrow"></div>
+                </div>
+                <div class="dropdown-content">
+                    <hr>
+                    <a href="#">Definisi, Jenis & Bentuk</a>
+                    <hr>
+                    <a href="#">Pencegahan & Penanganan</a>
+                    <hr>
+                    <a href="#">Dampak</a>
+                    <hr>
+                    <a href="#">Bantuan</a>
+                    <hr>
+                </div>
+            </li>
+        </ul>
+        <div class="auth-buttons">
+            <a class="loginbtn" href="#">Masuk</a>
+            <a class="registerbtn" href="#">Daftar</a>
+        </div>
+    </nav>
+    <!-- Nav -->
+    <!-- Hero -->
+    <section class="heropage grid2c">
+        <div class="header-wrap">
+            <div class="title-wrap">
+                <span class="bersamakita">Bersama Kita</span><span class="hentikan"> Hentikan Kekerasan Seksual!</span>
+            </div>
+            <p class="subtitle">Selamat datang di platform kami yang didedikasikan untuk memberikan himbauan, informasi, dan sumber daya bagi mereka yang ingin berpartisipasi dalam perjuangan melawan kekerasan seksual.</p>
+            <button class="btnhero" type="button">Lapor Kekerasan!</button>
+        </div>
+        <div class="heroimg-wrap">
+            <img class="heroimg" src="Assets/Hero/Beranda.png" />
+        </div>
+    </section>
+    <!-- Hero -->
+    <!-- Info -->
+    <section class="info pv">
+        <div class="info-wrap">
+            <div class="info-title">Informasi Terkait</div>
+            <div class="info-subtitle">Berikut merupakan beberapa panduan materi terkait Kekerasan Seksual</div>
+        </div>
+        <div class="panduan-wrap">
+            <div class="panduan">
+                <div class="icon-wrap">
+                    <div class="circle"></div>
+                    <img class="panduan-icon" src="Assets/Beranda/Icon 100x100px/Definisi.png" />
+                </div>
+                <div class="panduan-title">Definisi</div>
+                <div class="isi">Terdiri dari Definisi, Jenis, dan Bentuk Kekerasan Seksual</div>
+                <div class="btn-wrap">
+                    <a href="#" class="seemorebtn">Lebih Lanjut</a>
+                </div>
+            </div>
+            <div class="verticalline"></div>
+            <div class="panduan">
+                <div class="icon-wrap">
+                    <div class="circle"></div>
+                    <img class="panduan-icon" src="Assets/Beranda/Icon 100x100px/Pencegahan.png" />
+                </div>
+                <div class="panduan-title">Pencegahan &Penanganan </div>
+                <div class="isi">Beberapa langkah pencegahan agar tidak terjadi keberulangan</div>
+                <div class="btn-wrap">
+                    <a href="#" class="seemorebtn">Lebih Lanjut</a>
+                </div>
+            </div>
+            <div class="verticalline"></div>
+            <div class="panduan">
+                <div class="icon-wrap">
+                    <div class="circle"></div>
+                    <img class="panduan-icon" src="Assets/Beranda/Icon 100x100px/Dampak.png" />
+                </div>
+                <div class="panduan-title">Bantuan</div>
+                <div class="isi">Beberapa langkah bantuan apabila terjadi kekerasan seksual</div>
+                <div class="btn-wrap">
+                    <a href="#" class="seemorebtn">Lebih Lanjut</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Info -->
+    <!--  -->
+    <section class="ajakan pv grid2c">
+        <div class="ajakanimg-wrap">
+            <div class="ajakanimg">
+                <div class="rectangle"></div>
+                <img class="img" src="Assets/Beranda/Foto 535x310px/Bersama_Hadapi.png" />
+            </div>
+        </div>
+        <div class="ajakanteks-wrap">
+            <div class="ajakanteks-title">Mari Bergerak Bersama!</div>
+            <div class="ajakanteks-content">Kemendikbudristek telah menerbitkan Permendikbudristek tentang Pencegahan dan Penanganan Kekerasan Seksual di Pendidikan Tinggi, sebagai komitmen serius dalam menjaga hak pendidikan dan menghentikan kekerasan seksual di lingkungan pendidikan. Langkah ini sejalan dengan Pasal 31 Undang-Undang Dasar Negara dan Tujuan Pembangunan Berkelanjutan, khususnya dalam mencapai tujuan pendidikan dan kesetaraan gender. Tujuannya adalah memastikan bahwa upaya pencegahan kekerasan seksual di institusi pendidikan tidak menghambat akses dan kelanjutan pendidikan bagi semua warga negara.</div>
+        </div>
+    </section>
+    <!--  -->
+    <!--  -->
+    <section class="himbauan pv">
+        <div class="himbauan-wrap">
+            <span class="himbauan-title">Anda tidak sendirian</span>
+            <Br></Br>
+            <span class="himbauan-content">Laporkanlah ke orang terdekat yang Anda percayai bila mengetahui atau mengalami Kekerasan Seksual. Jangan takut untuk melapor, karena Anda berhak membela hak asasi Anda yang telah dilanggar.</span>
+        </div>
+    </section>
+    <!--  -->
+    <!--  -->
+    <div class="bantuan pv">
+        <div class="bantuantitle-wrap">
+            <div class="bantuantitle">Bantuan dalam Kekerasan Seksual </div>
+            <div class="bantuansubtitle">Berikut merupakan beberapa panduan dalam menangani kekerasan seksual</div>
+        </div>
+        <div class="card-wrap grid3c pv">
+            <div class="card">
+                <div class="cardicon-wrap">
+                    <div class="circle"></div>
+                    <img class="card-icon" src="https://via.placeholder.com/66x66" />
+                </div>
+                <div class="card-body">
+                    <div class="card-title">Cari Tahu</div>
+                    <div class="card-content">Cari tahu bila Anda mengalami kekerasan seksual.
+                    </div>
+                </div>
+                <button class="btnbantuan" type="button">Mendapatkan Bantuan</button>
+            </div>
+            <div class="card">
+                <div class="cardicon-wrap">
+                    <div class="circle"></div>
+                    <img class="card-icon" src="https://via.placeholder.com/66x66" />
+                </div>
+                <div class="card-body">
+                    <div class="card-title">Prinsip Membantu</div>
+                    <div class="card-content">Cari tahu prinsip-prinsip dalam membantu korban.</div>
+                </div>
+                <button class="btnbantuan" type="button">Membantu Korban</button>
+            </div>
+            <div class="card">
+                <div class="cardicon-wrap">
+                    <div class="circle"></div>
+                    <img class="card-icon" src="https://via.placeholder.com/66x66" />
+                </div>
+                <div class="card-body">
+                    <div class="card-title">Layanan Konseling</div>
+                    <div class="card-content">Cari tahu rekomendasi layanan konseling.</div>
+                </div>
+                <button class="btnbantuan" type="button">Untuk Penyintas</button>
+            </div>
+        </div>
+    </div>
+    <!--  -->
+    <!--  -->
+
+    <!--  -->
+    <script src="script.js"></script>
+</body>
+
+</html>
